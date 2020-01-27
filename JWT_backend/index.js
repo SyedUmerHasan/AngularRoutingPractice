6c96200f264cf458b573d030d8ed1d202ffdb32e
Syed Umer Hasan <umerhasan2010@gmail.com>
2020-01-27 09:53:47 +0500

    adding backend using jwt

---
+++ JWT_backend/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+var cors = require('cors')
+
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.use(cors())
+
+
+let userList = [];
+let loginUser = [];
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Welcome to the API'
+    });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.json({
+                message: 'Post not created...',
+                error: err
+            });
+
+            // res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
+        // res.send({ "json": req.token })
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    // Mock user
+    const user = {
+        id: 1,
+        username: 'brad',
+        email: 'brad@gmail.com'
+    }
+
+    jwt.sign({ user }, 'secretkey', { expiresIn: '2 days' }, (err, token) => {
+        res.json({
+            token
+        });
+    });
+
+});
+
+
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        console.log(req.headers);
+    }
+
+}
+
+app.listen(5000, () => console.log('Server started on port 5000'))
